90b856f65c3a253c65524439f1b498736820eb86
Brad Rumack <brad.rumack@gmail.com>
2020-10-26 20:34:53 -0700

    move to gsap

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
-new CircleType(document.getElementById('coming'))
+document.addEventListener("DOMContentLoaded", function(){
+    
+    new CircleType(document.getElementById('coming'))
     .radius(300)
 
-new CircleType(document.getElementById('soon'))
-    .dir(-1)
-    .radius(300)
+    new CircleType(document.getElementById('soon'))
+        .dir(-1)
+        .radius(300)
+
+    gsap.to('#heroLogo', { width: '50%', rotation: 720, duration: .75 })
+    gsap.to('#outerCircleBorder', { rotation: 360, duration: 30, repeat: -1, ease: 'linear' })
+    gsap.to('#innerCircleBorder', { rotation: -360, duration: 50, repeat: -1, ease: 'linear'})
+    gsap.to('#coming', {opacity: 1, delay: 1})
+    gsap.to('#soon', {opacity: 1, delay: 1})
+});
+
